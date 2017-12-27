e1f8cb874a90e35f596ce49efa5e762712821735
Obaied <abdullah.obaied@gmail.com>
2017-12-27 14:07:37 +0100

    Replace SDK_DIR with ROOT_DIR for better clarity in build script

--- ext/ios/build.sh
+++ ext/ios/build.sh
@@ -4,27 +4,27 @@
 set -e
 
 # Get the current directory
-SDK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Traverse up to get to the root directory
-SDK_DIR="$(dirname "$SDK_DIR")"
-SDK_DIR="$(dirname "$SDK_DIR")"
+ROOT_DIR="$(dirname "$ROOT_DIR")"
+ROOT_DIR="$(dirname "$ROOT_DIR")"
 
 SRC_DIR=ext/ios/sdk
-LIB_OUT_DIR=ios/iphone/platform
+LIB_OUT_DIR=ios/ios/platform
 
 RED='\033[0;31m' # Red color
 GREEN='\033[0;32m' # Green color
 NC='\033[0m' # No Color
 
-cd ${SDK_DIR}
+cd ${ROOT_DIR}
 echo -e "${GREEN}#### Removing old Adjust iOS framework ${NC}"
 rm -rfv ${LIB_OUT_DIR}/AdjustSdk.framework
 
 echo -e "${GREEN}#### Building new Adjust iOS framework ${NC}"
-cd ${SDK_DIR}/${SRC_DIR}
+cd ${ROOT_DIR}/${SRC_DIR}
 xcodebuild -target AdjustStatic -configuration Release clean build
 
 echo -e "${GREEN}#### Copy newly built Adjust iOS framework to designated location ${NC}"
-cd ${SDK_DIR}
+cd ${ROOT_DIR}
 cp -Rv ${SRC_DIR}/Frameworks/Static/AdjustSdk.framework ${LIB_OUT_DIR}
